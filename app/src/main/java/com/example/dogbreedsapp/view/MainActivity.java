b661b92a582d0cb7bfda387f1e9ac26bf323ec82
Horacio <horacio.junior@ibm.com>
2022-03-10 07:14:26 -0300

    initial fragments

--- app/src/main/java/com/example/dogbreedsapp/view/MainActivity.java
+++ app/src/main/java/com/example/dogbreedsapp/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.dogbreedsapp;
+package com.example.dogbreedsapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.dogbreedsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
