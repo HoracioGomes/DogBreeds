6f3341f1858e7d822afd20491cb36b580e229e80
Horacio <horacio.junior@ibm.com>
2022-04-20 05:44:08 -0300

    Implementacao inicial ListFragment

--- app/src/main/java/com/example/dogbreedsapp/view/ListFragment.java
+++ app/src/main/java/com/example/dogbreedsapp/view/ListFragment.java
@@ -4,20 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.dogbreedsapp.R;
+import com.example.dogbreedsapp.viewmodel.ListViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ListFragment extends Fragment {
+
+    private ListViewModel listViewModel;
+    private DogListAdapter adapter = new DogListAdapter(new ArrayList<>());
+
+    @BindView(R.id.refresh_layout_list_dog)
+    private SwipeRefreshLayout refreshLayout;
+    @BindView(R.id.recycler_view_list_dog)
+    private RecyclerView recyclerView;
+    @BindView(R.id.progress_bar_list_dog)
+    private ProgressBar loading;
+    @BindView(R.id.text_view_error_list_dog)
+    private TextView error;
 
     public ListFragment() {
     }
@@ -30,4 +51,11 @@ public class ListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listViewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+    }
 }
