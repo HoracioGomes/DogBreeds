d1fed0f0058fdbbe9d9efa38cccb436b71007f48
Horacio <horacio.junior@ibm.com>
2022-05-02 05:30:31 -0300

    Implementacao RxJava com retrofit para buscar lista de racas

--- app/src/main/java/com/example/dogbreedsapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/dogbreedsapp/viewmodel/ListViewModel.java
@@ -7,33 +7,60 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.dogbreedsapp.model.DogBreed;
+import com.example.dogbreedsapp.retrofit.DogsService;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class ListViewModel extends AndroidViewModel {
     public MutableLiveData<List<DogBreed>> dogs = new MutableLiveData<List<DogBreed>>();
     public MutableLiveData<Boolean> dogLoadError = new MutableLiveData<Boolean>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+    private DogsService dogsService = new DogsService();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public ListViewModel(@NonNull Application application) {
         super(application);
     }
 
     public void refresh() {
-        DogBreed dog1 = new DogBreed("1", "Pincher", "12", "", "", "", "");
-        DogBreed dog2 = new DogBreed("2", "Pitbull", "12", "", "", "", "");
-        DogBreed dog3 = new DogBreed("3", "Fila", "10", "", "", "", "");
-        DogBreed dog4 = new DogBreed("4", "Labrador", "15", "", "", "", "");
+        getFromRemote();
+    }
 
-        ArrayList<DogBreed> dogList = new ArrayList<DogBreed>();
-        dogList.add(dog1);
-        dogList.add(dog2);
-        dogList.add(dog3);
-        dogList.add(dog4);
+    private void getFromRemote() {
+        loading.setValue(true);
+        compositeDisposable.add(
+                dogsService.getDogBreeds()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<DogBreed>>() {
+                            @Override
+                            public void onSuccess(@NonNull List<DogBreed> dogBreeds) {
+                                dogs.setValue(dogBreeds);
+                                dogLoadError.setValue(false);
+                                loading.setValue(false);
+                            }
 
-        dogs.setValue(dogList);
-        dogLoadError.setValue(false);
-        loading.setValue(false);
+                            @Override
+                            public void onError(@NonNull Throwable e) {
+                                dogLoadError.setValue(true);
+                                loading.setValue(false);
+                                e.printStackTrace();
+                            }
+
+                        })
+
+        );
+
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.clear();
     }
 }
