d2ff7396728c268bed619f2dc4ab26b8e852cd08
Horacio <horacio.junior@ibm.com>
2022-09-16 08:54:49 -0300

    Criacao do canal de notificacoes para racas recebidas

--- app/src/main/java/com/example/dogbreedsapp/util/NotificationHelper.java
+++ app/src/main/java/com/example/dogbreedsapp/util/NotificationHelper.java
@@ -1,10 +1,16 @@
 package com.example.dogbreedsapp.util;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 
 public class NotificationHelper {
     private static NotificationHelper instance;
     private Context context;
+
+    private static final String CHANNEL_ID = "Dogs-Cannel";
+    private static final int NOTIFICATION_ID = 123;
 
     private NotificationHelper(Context context) {
         this.context = context;
@@ -15,6 +21,19 @@ public class NotificationHelper {
             instance = new NotificationHelper(context);
         }
         return instance;
+    }
+
+    private void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = CHANNEL_ID;
+            String description = "canal das notificacoes de racas recebidas";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+        }
+
     }
 
 }
